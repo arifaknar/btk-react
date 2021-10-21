fd94cedd8c6d7deb725a0326d83fb809c96b21ed
Arif AKNAR <arifaknar@gmail.com>
2021-10-21 15:46:25 +0300

    Redux-intro-small-project
    Signed-off-by: Arif AKNAR <arifaknar@gmail.com>

---
+++ redux-intro/src/App.js
@@ -0,0 +1,18 @@
+
+import Counter from './components/Counter';
+import IncreaseCounter from './components/IncreaseCounter';
+import DecreaseCounter from './components/DecreaseCounter';
+import IncreaseByTwoCounter from './components/IncreaseByTwoCounter';
+
+function App() {
+  return (
+    <>
+      <Counter/>
+      <IncreaseCounter/>
+      <DecreaseCounter/>
+      <IncreaseByTwoCounter/>
+    </>
+  );
+}
+
+export default App;
